abfef53432126474a662fc9475d3603636dd22f1
Tolani <110635435+Babanfada@users.noreply.github.com>
2022-12-10 21:26:08 +0000

    commit

--- Components/Layout.jsx
+++ Components/Layout.jsx
@@ -1,13 +1,61 @@
 import Navigation from "./Navigation";
+import MenuIcon from "@mui/icons-material/Menu";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import PersonIcon from "@mui/icons-material/Person";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import HomeIcon from "@mui/icons-material/Home";
+import { createContext, useState } from "react";
+export const dataContext = createContext("");
+
 const Layout = ({ children }) => {
+  const icon = [
+    {
+      icon: <MenuIcon sx={{ fonstSize: "50" }} />,
+      name: "Menu",
+    },
+    {
+      icon: <HomeIcon sx={{ fonstSize: "50" }} />,
+      name: "Home",
+    },
+    {
+      icon: <LightModeIcon sx={{ fonstSize: "50" }} />,
+      name: "Light",
+    },
+    {
+      icon: <DarkModeIcon sx={{ fonstSize: "50" }} />,
+      name: "Dark",
+    },
+    {
+      icon: <PersonIcon sx={{ fonstSize: "50" }} />,
+      name: "Person",
+    },
+    {
+      icon: <BusinessCenterIcon sx={{ fonstSize: "50" }} />,
+      name: "portfolio",
+    },
+    {
+      icon: <ContactPhoneIcon sx={{ fonstSize: "50" }} />,
+      name: "contact",
+    },
+  ];
   return (
-    <div>
-      <div style={{padding:"2vw"}}>
-        <Navigation />
+    <dataContext.Provider value={{ icon }}>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Navigation />
+        </div>
+        <div>{children}</div>
+        <div>footer</div>
       </div>
-      <div>{children}</div>
-      <div>footer</div>
-    </div>
+    </dataContext.Provider>
   );
 };
 
--- Components/Navigation.jsx
+++ Components/Navigation.jsx
@@ -1,31 +1,25 @@
-import React from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import styles from "../styles/Navigation.module.css";
 import { Avatar } from "@mui/material";
-
-const style1 = {
-  border: "1px solid red",
-  display: "flex",
-  justifyContent: "flex-end",
-  alignItems: "center",
-  width: "150px",
-  height: "50px",
-  padding: "5px 15px 5px 15px",
-  gap: "3vw",
-  borderRadius: "25px",
-};
+import { dataContext } from "./Layout";
+import { useContext } from "react";
 
 const Navigation = () => {
+  const { icon } = useContext(dataContext);
+  const [Menu, Home, , , , , contact] = icon;
+
   return (
-    <div style={style1}>
-      <div
-        className={(styles.icon)}
-        style={{ fontSize: "larger" }}
+    <div>
+      <Avatar
+        sx={{ width: "60px", height: "60px" }}
+        style={{
+          padding: "10px",
+          cursor: "pointer",
+          backgroundColor: "rgba(255, 255, 255, 0.109)",
+        }}
       >
-        Home
-      </div>
-      <Avatar>
-        <HomeIcon sx={{ fonstSize: "40" }} />
+        {/* <HomeIcon /> */}
+        {contact.icon}
       </Avatar>
     </div>
   );
