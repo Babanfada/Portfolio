ea8ff4955d60b4b901274d1f9ddd9a8de16a18ca
Tolani <110635435+Babanfada@users.noreply.github.com>
2022-12-27 10:41:45 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Meta from "../public/Meta";
 import styles from "../styles/Home.module.css";
-import hort from "../public/images/fullscreendark.jpg";
+// import hort from "../public/images/fullscreendark.jpg";
 import Detailsme from "../Components/Detailsme";
 import { motion } from "framer-motion";
 export default function Home() {
