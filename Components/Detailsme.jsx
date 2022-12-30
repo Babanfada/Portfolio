ae0361907a459b026e00b9fa52bc940e484aa583
Tolani <110635435+Babanfada@users.noreply.github.com>
2022-12-30 16:21:33 +0000

    commit

--- Components/Detailsme.jsx
+++ Components/Detailsme.jsx
@@ -103,7 +103,7 @@ const Detailsme = () => {
             className={styles.stackbutton}
             title="Download CV"
             download="Curriculum Vitae"
-            href={"document.pdf"}
+            href={"portfolio.pdf"}
             target={"blank"}
           >
             <BootstrapButton
